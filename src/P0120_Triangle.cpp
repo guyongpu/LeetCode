ee4021022cf2cb841b6279a510b339642fc32fe0
guyongpu <gyphust@qq.com>
2019-11-23 22:13:04 +0800

    P0119 finish

--- src/P0120_Triangle.cpp
+++ src/P0120_Triangle.cpp
@@ -4,11 +4,20 @@
 
 #include "P0120_Triangle.h"
 
+void dfs(vector<vector<int>> &triangle, int pos_i, int pos_j, int temp, int &result) {
+    if (pos_i == triangle.size()) {
+        result = min(result, temp);
+    } else {
+
+    }
+
+
+}
+
 int P0120_Triangle::minimumTotal(vector<vector<int>> &triangle) {
-    
+    int result = INT_MAX;
 
-
-
+    dfs(triangle, 0, 0, 0, result);
 
     return 0;
 }
